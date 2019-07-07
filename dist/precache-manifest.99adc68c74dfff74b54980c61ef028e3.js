a6be4101a17072ca2933b067367def4120c03859
nromano <nromano@utk.edu>
2019-07-07 17:55:40 -0400

    added missing dist files

---
+++ dist/precache-manifest.99adc68c74dfff74b54980c61ef028e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/alterlog.io/robots.txt"
+  },
+  {
+    "revision": "34ca2b7d55baba53dbe2",
+    "url": "/alterlog.io/js/chunk-vendors.c6d8d353.js"
+  },
+  {
+    "revision": "85e3bb553d030204cb64",
+    "url": "/alterlog.io/js/app.a9d2cde3.js"
+  },
+  {
+    "revision": "e074f0caae32dfcdc367",
+    "url": "/alterlog.io/js/about.21d842e6.js"
+  },
+  {
+    "revision": "27669fca4f78c68b41d2e40e8a249e37",
+    "url": "/alterlog.io/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/alterlog.io/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "34ca2b7d55baba53dbe2",
+    "url": "/alterlog.io/css/chunk-vendors.922b272a.css"
+  }
+];
