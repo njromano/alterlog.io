7393f6e86016aa7774a7d1413726dd20c03bf8b4
nromano <nromano@utk.edu>
2019-07-07 16:28:16 -0400

    updated baseurl

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/alterlog.io'
+    baseUrl: process.env.NODE_ENV === 'production'
+        ? '/alterlog.io/'
         : '/'
 }
