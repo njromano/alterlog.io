e765c305a7e5a05bf6393d107d424431cfeefb25
Nick Romano <dev@njromano.com>
2019-07-07 20:18:26 -0400

    Fixed cancel button

---
+++ dist/precache-manifest.45d9da80697a71d5528b493f987817e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/alterlog.io/robots.txt"
+  },
+  {
+    "revision": "86a72a9af2946d02a861",
+    "url": "/alterlog.io/js/chunk-vendors.90ea3d15.js"
+  },
+  {
+    "revision": "84ef570d9344986ddaf2",
+    "url": "/alterlog.io/js/app.8fa62437.js"
+  },
+  {
+    "revision": "e074f0caae32dfcdc367",
+    "url": "/alterlog.io/js/about.21d842e6.js"
+  },
+  {
+    "revision": "b6004aca5780d2ffb79c2650f7178ecc",
+    "url": "/alterlog.io/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/alterlog.io/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "86a72a9af2946d02a861",
+    "url": "/alterlog.io/css/chunk-vendors.c6908b88.css"
+  }
+];
