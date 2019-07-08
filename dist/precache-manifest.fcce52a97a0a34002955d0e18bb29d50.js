d6f09c84ab917de7f3e01f4a68968b220eba3d28
nromano <nromano@utk.edu>
2019-07-07 23:01:14 -0400

    Updated secondary color

---
+++ dist/precache-manifest.fcce52a97a0a34002955d0e18bb29d50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/alterlog.io/robots.txt"
+  },
+  {
+    "revision": "318bd7c52e48b9c04a47",
+    "url": "/alterlog.io/js/chunk-vendors.4087758c.js"
+  },
+  {
+    "revision": "134402098d1692646622",
+    "url": "/alterlog.io/js/app.f652f96f.js"
+  },
+  {
+    "revision": "a6c8b916f9dafaf4535d",
+    "url": "/alterlog.io/js/about.cc9732e6.js"
+  },
+  {
+    "revision": "fc78302af4105e6f7ab9584093f00258",
+    "url": "/alterlog.io/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/alterlog.io/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "318bd7c52e48b9c04a47",
+    "url": "/alterlog.io/css/chunk-vendors.58c540d7.css"
+  },
+  {
+    "revision": "134402098d1692646622",
+    "url": "/alterlog.io/css/app.f2e9aa6b.css"
+  }
+];
