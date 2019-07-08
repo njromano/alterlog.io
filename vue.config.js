3238c84e6f014788cfe09acb8be44702e6797541
nromano <nromano@utk.edu>
2019-07-07 22:52:13 -0400

    updated theme color in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/alterlog.io/'
-        : '/'
+        : '/',
+    pwa: {
+        themeColor: "#699947"
+    }
 }
